dd70afa33ceb5861a7d54748b99b9f395493a4e2
VHarbar <vharbar64@gmail.com>
2022-08-24 16:01:41 +0300

    implemented release version of login form, also user verification was implemented

--- Phoenix/authorization/urls.py
+++ Phoenix/authorization/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from . import views
 
+app_name = 'authorize'
+
 urlpatterns = [
-    path('', views.authorize, name="authorization"),
+    path('', views.authorize_view, name="authorization"),
 ]
--- Phoenix/authorization/views.py
+++ Phoenix/authorization/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
-def authorize(request):
-    return render(request, "authorization/authorization.html")
+def authorize_view (request):
+    if request.method == 'POST':
+        username = request.POST.get('login')
+        password = request.POST.get('pass')
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return HttpResponse('Authenticated successfully')
+    context = {}
+    return render(request, 'authorization/authorization.html', context)
